6cd77515d2000c8684e583fd4e367ef9f43f04af
Mr. Kuldeep <128669361+MrKuldeep01@users.noreply.github.com>
2023-05-25 13:27:39 +0530

    Delete codeEditor directory

--- codeEditor/script.js
+++
@@ -1,8 +0,0 @@
-function run(){
-    let htmlCode = document.getElementById("htmlcode").value;
-    let cssCode = document.getElementById("csscode").value;
-    let jsCode = document.getElementById("jscode").value;
-    let output = document.getElementById("output");
-    output.contentDocument.body.innerHTML= htmlCode +"<style>"+cssCode+"</style>";
-    output.contentWindow.eval(jsCode);
-}
